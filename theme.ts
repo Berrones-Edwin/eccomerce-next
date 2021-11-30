d748c1e3295e33a4a45986d5d722e0e7c2ee8748
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-11-30 10:47:02 -0600

    [Refactor] Apply eslint and prettier

--- theme.ts
+++ theme.ts
@@ -1,14 +1,14 @@
 import { extendTheme, theme } from '@chakra-ui/react'
 import { INFORMATION } from './app/constants'
 export default extendTheme({
-    colors: {
-        primary: theme.colors[INFORMATION.color]
-    },
-    styles: {
-        global: {
-            body: {
-                backgroundColor: 'primary.50'
-            }
-        }
+  colors: {
+    primary: theme.colors[INFORMATION.color]
+  },
+  styles: {
+    global: {
+      body: {
+        backgroundColor: 'primary.50'
+      }
     }
+  }
 })
